ecbd3afde774fbb54b37ace860dd73b6a02f8a70
mragicl <mragicl@protonmail.com>
2021-12-20 21:14:48 +0100

    script to convert the keystore files to validator wallet for lighthouse

---
+++ lighthousebeacon/initialize_validator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yes "" | /usr/local/bin/lighthouse account validator import --directory /data/app/assigned_data/keys  --datadir /var/lib/lighthouse --testnet-dir /var/lib/lighthouse/kintsugi-testnet/custom_config_data
